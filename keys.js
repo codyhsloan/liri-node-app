50ef12a75150259c7970f64ee8c9ed39e06bd2d9
Cody Sloan <chsloan@ncsu.edu>
2017-08-28 15:14:52 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'N4ifSRdshVH3caNTScNwGXbZp',
+  consumer_secret: 'sujFmyrhU5ark0j9L6i3CXVRZFlIEj9qtKgYh8OfC7dW5ppoon',
+  access_token_key: '	1012364017-MrJ6tLRYTqbvadkmEM359wRRZfAuFu22FVyM0VT',
+  access_token_secret: 'n0z0UM6xAY0o9YZ6288RpPbre57J0m5BY4ckn8cGDL9mn',
+}
---
+++ liri.js
@@ -0,0 +1,9 @@
+var Twitter = require('twitter');
+
+var keys = require('./keys.js');
+
+			/*"my-tweets"
+			"spotify-this-song"
+			"movie-this"
+			"do-what-it-says"*/
+
